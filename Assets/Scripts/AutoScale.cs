d6bf11dffa9649fff71bb8b91157695ae1988dbf
Nguyen Nam Phuong <nnamphuong2004@gmail.com>
2025-03-12 19:15:15 +0700

    Update files

---
+++ Assets/Scripts/AutoScale.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScale : MonoBehaviour
+{ 
+     private void Start()
+     {
+          ScaleObject();
+     }
+
+     private void ScaleObject()
+     {
+        float width = ((float)16/9) / ((float)Screen.width / (float)Screen.height);
+       // float height = ((float)9/16) / ((float)Screen.height / (float)Screen.width);
+        transform.localScale = new Vector3(transform.localScale.x / width, transform.localScale.y, transform.localScale.z);
+     }
+}
+  
